347a3515b9d6223f82af51be8fe25eed4ce87dad
kevin <kevin@otto>
2012-07-13 18:14:19 +0200

    First Model

---
+++ application/models/User.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Description of User
+ *
+ * @author Kevin Purrmann
+ */
+class Application_Model_User {
+	//put your code here
+}
+
+?>
---
+++ tests/application/models/UserTest.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * TestClass for UserModel
+ *
+ * @author Kevin Purrmann
+ */
+class Application_Model_UserTest extends Zend_Test_PHPUnit_ControllerTestCase {
+
+	public function setUp() {
+		$this->bootstrap = new Zend_Application(APPLICATION_ENV, APPLICATION_PATH . '/configs/application.ini');
+		parent::setUp();
+	}
+
+	public function testCanCreateUser() {
+		$user = new Application_Model_User();
+		$this->assertInstanceOf('Application_Model_User', $user);
+	}
+
+}
+
+?>
